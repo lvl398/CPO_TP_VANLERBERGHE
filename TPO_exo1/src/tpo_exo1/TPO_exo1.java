41d2e82863496cf4a6806107b0f3e4cd1e50e8d1
louison_vl <louison_vl@localhost>
2024-09-17 12:55:02 +0200

    TPO avancement jusqu'à étape 5 page 7

--- TPO_exo1/src/tpo_exo1/TPO_exo1.java
+++ TPO_exo1/src/tpo_exo1/TPO_exo1.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TPO Exercice 1
+ * TDB
+ * Septembre 2024
  */
 package tpo_exo1;
 
@@ -14,7 +15,12 @@ public class TPO_exo1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc : new Scanner(System.in);
+        System.out.println("Bonjour,quel est votre prénom?");
     }
     
 }
