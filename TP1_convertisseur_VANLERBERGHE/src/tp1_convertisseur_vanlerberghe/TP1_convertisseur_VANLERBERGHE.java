55a02fa973287f6f529b461038493ff075dcb488
louison_vl <louison_vl@localhost>
2024-09-20 18:30:09 +0200

    Exercice 2 TP1 (début)

---
+++ TP1_convertisseur_VANLERBERGHE/src/tp1_convertisseur_vanlerberghe/TP1_convertisseur_VANLERBERGHE.java
@@ -0,0 +1,21 @@
+/*
+ * TP1 Exercice 2 
+ * TDB
+ * Septembre 2024
+ */
+package tp1_convertisseur_vanlerberghe;
+
+/**
+ *
+ * @author louison_vl
+ */
+public class TP1_convertisseur_VANLERBERGHE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
