34928a21819a3c4da46b720e699bc62c109a8d28
louison_vl <louison_vl@localhost>
2024-09-27 10:56:11 +0200

    exercice 3 terminé

--- TP1_guessMyNumber_VANLERBERGHE/src/tp1_guessmynumber_vanlerberghe/TP1_guessMyNumber_VANLERBERGHE.java
+++ TP1_guessMyNumber_VANLERBERGHE/src/tp1_guessmynumber_vanlerberghe/TP1_guessMyNumber_VANLERBERGHE.java
@@ -29,26 +29,70 @@ public class TP1_guessMyNumber_VANLERBERGHE {
         Scanner sc=new Scanner(System.in);
         System.out.println("Choisir un mode de difficulté "+"\n"+"1) facile"+"\n"+"2) normal"+"\n"+"3) difficile");
         int choix=sc.nextInt();
+        
+        // jeu mode facile 
         if (choix==1){
-            diff=n-nombre;
-            if (diff>20)
-        } else if (choix==2) {
-            
-        } else if (choix==3) {
-            
-        }
-        while (nombre!=n) {
-            System.out.println("Choisissez un nombre entre 0 et 100");
-            nombre=sc.nextInt();
-            if (nombre>n) {
-                System.out.println("trop grand");
-            } else if (nombre<n) {
-                System.out.println("trop petit");
-            } else {
-                System.out.println("égal");
+            while (nombre!=n) {
+                System.out.println("Choisissez un nombre entre 0 et 100");
+                nombre=sc.nextInt();
+                diff=n-nombre;
+                if (diff>30){
+                    System.out.println("vraiment trop petit");
+                    i=i+1;
+                } else if (diff<-30) {
+                    System.out.println("vraiment trop grand");
+                    i=i+1;
+                } else {
+                    if (nombre>n) {
+                        System.out.println("trop grand");
+                    } else if (nombre<n) {
+                        System.out.println("trop petit");
+                    } else {
+                        System.out.println("égal");
+                    }
+                    i=i+1;
+                }
+                
             }
-            i=i+1;
-     
+            System.out.println("Le nombre de tentatives est "+i);
+            
+        // jeu mode normal
+        } else if (choix==2) {
+            while (nombre!=n) {
+                System.out.println("Choisissez un nombre entre 0 et 100");
+                nombre=sc.nextInt();
+                if (nombre>n) {
+                    System.out.println("trop grand");
+                } else if (nombre<n) {
+                    System.out.println("trop petit");
+                } else {
+                    System.out.println("égal");
+                }
+                i=i+1;
+            }
+            System.out.println("Le nombre de tentatives est "+i);
+            
+        // jeu mode difficile
+        } else if (choix==3) {
+            int tentative=5;
+            while (nombre!=n) {
+                System.out.println("Il vous reste "+tentative);
+                System.out.println("Choisissez un nombre entre 0 et 100");
+                nombre=sc.nextInt();
+                if (nombre>n) {
+                    System.out.println("trop grand");
+                } else if (nombre<n) {
+                    System.out.println("trop petit");
+                } else {
+                    System.out.println("égal");
+                }
+                i=i+1;
+                tentative=tentative-1;
+                if (tentative==0) {
+                    System.out.println("Vous avez perdu!");
+                    nombre=n;
+                }
+            }
         }
         System.out.println("Le nombre de tentatives est "+i);
         }
