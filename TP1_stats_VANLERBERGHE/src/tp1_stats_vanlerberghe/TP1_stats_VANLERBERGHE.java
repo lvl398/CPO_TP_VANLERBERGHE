76f20c8e5c5c304af9d7814d8a01892e327c0677
louison_vl <louison_vl@localhost>
2024-09-27 10:59:21 +0200

    début exercice 4 TP1

---
+++ TP1_stats_VANLERBERGHE/src/tp1_stats_vanlerberghe/TP1_stats_VANLERBERGHE.java
@@ -0,0 +1,21 @@
+/*
+ * TP1 Exercice 4
+ * TDB
+ * Septembre 2024
+ */
+package tp1_stats_vanlerberghe;
+
+/**
+ *
+ * @author louison_vl
+ */
+public class TP1_stats_VANLERBERGHE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
