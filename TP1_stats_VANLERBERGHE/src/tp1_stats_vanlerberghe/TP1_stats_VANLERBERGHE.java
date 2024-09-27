e34faa78552cbef0f521642f20fb9192a3dda48f
louison_vl <louison_vl@localhost>
2024-09-27 11:16:27 +0200

    manque pourcentage en double

--- TP1_stats_VANLERBERGHE/src/tp1_stats_vanlerberghe/TP1_stats_VANLERBERGHE.java
+++ TP1_stats_VANLERBERGHE/src/tp1_stats_vanlerberghe/TP1_stats_VANLERBERGHE.java
@@ -5,6 +5,9 @@
  */
 package tp1_stats_vanlerberghe;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author louison_vl
@@ -15,7 +18,19 @@ public class TP1_stats_VANLERBERGHE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int [] tab=new int[6];
+        Scanner nbr=new Scanner(System.in);
+        System.out.println("Saisir un nombre entier");
+        int m=nbr.nextInt();
+        for (int i=0; i<m; i++) {
+            Random generateurAleat = new Random ();
+            int n = generateurAleat.nextInt(5);
+            tab[n]++;
+        }
+        for (int k=0; k<6; k++) {
+            tab[k]=(tab[k]*100)/m;
+            System.out.println("cellule "+ k +" : "+tab[k]+"%");
+        }
     }
     
 }
