a37bc37b093a7826468d5e3079f4b953908f88bf
louison_vl <louison_vl@localhost>
2024-10-07 16:09:00 +0200

    début exercice 1 TP2

---
+++ TP2_Bieres_VANLERBERGHE/src/tp2_bieres_vanlerberghe/TP2_Bieres_VANLERBERGHE.java
@@ -0,0 +1,21 @@
+/*
+ * TP2 Exercice 1
+ * TDB
+ * Octobre 2024
+ */
+package tp2_bieres_vanlerberghe;
+
+/**
+ *
+ * @author louison_vl
+ */
+public class TP2_Bieres_VANLERBERGHE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
