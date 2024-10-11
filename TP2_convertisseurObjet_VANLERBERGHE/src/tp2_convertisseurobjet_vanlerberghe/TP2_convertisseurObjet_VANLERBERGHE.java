7723a2f0fc4cee1cf4a84d715ab1e3b6cd2fbc68
louison_vl <louison_vl@localhost>
2024-10-11 09:28:25 +0200

    exercice 2 du TP2 étape 5

--- TP2_convertisseurObjet_VANLERBERGHE/src/tp2_convertisseurobjet_vanlerberghe/TP2_convertisseurObjet_VANLERBERGHE.java
+++ TP2_convertisseurObjet_VANLERBERGHE/src/tp2_convertisseurobjet_vanlerberghe/TP2_convertisseurObjet_VANLERBERGHE.java
@@ -31,10 +31,11 @@ public class TP2_convertisseurObjet_VANLERBERGHE {
         temperature.FarenheitVersCelcius(nombre);
         System.out.println(temperature);
         
+        while 
         // On demande à l'utilisateur une conversion qu'il souhaite faire
         Scanner conv=new Scanner(System.in);
         System.out.println("Saisissez la conversion que vous souhaiter effectuer :"+"\n"+"1) De Celcius vers Kelvin"+"\n"+"2) De Kelvin vers Celcius");
-        System.out.println("3) De Celcius vers Farenheit"+"\n"+"4) De Farenheit vers Celcius"+"\n"+"5) De Kelvin vers Farenheit"+"\n"+"6) De Farenheit vers Kelvin");
+        System.out.println("3) De Celcius vers Farenheit"+"\n"+"4) De Farenheit vers Celcius"+"\n"+"5) De Kelvin vers Farenheit"+"\n"+"6) De Farenheit vers Kelvin"+"\n"+"0) Plus besoin de convertir");
         double conversion;
         conversion=conv.nextDouble();
     }
