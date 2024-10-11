c809727b709391ef9f75e9f7350ec62db205613b
louison_vl <louison_vl@localhost>
2024-10-11 08:51:41 +0200

    exercice 2 du TP2 étape 4

---
+++ TP2_convertisseurObjet_VANLERBERGHE/src/tp2_convertisseurobjet_vanlerberghe/Convertisseur.java
@@ -0,0 +1,46 @@
+/*
+ * TP2 Exercice 2
+ * TDB
+ * Octobre 2024
+ */
+package tp2_convertisseurobjet_vanlerberghe;
+
+/**
+ *
+ * @author louison_vl
+ */
+public class Convertisseur {
+    int nbConversions=0;
+    
+    @Override
+    public String toString () {
+        return "nb de conversions"+ nbConversions;
+    }
+    
+    public double CelciusVersKelvin (double tCelcius) {
+        tCelcius=tCelcius+273;
+        return tCelcius;
+    }
+    public double KelvinVersCelcius (double tKelvin) {
+        tKelvin=tKelvin-273;
+        return tKelvin;
+    }
+    public double CelciusVersFarenheit (double tCelcius) {
+        tCelcius=tCelcius*9/5+32;
+        return tCelcius;
+    }
+    public double FarenheitVersCelcius (double tFarenheit) {
+        tFarenheit=(tFarenheit-32)*5/9;
+        return tFarenheit;
+    }
+    public double KelvinVersFarenheit (double tKelvin) {
+        tKelvin=(tKelvin-273)*9/5+32;
+        return tKelvin;
+    }
+    public double FarenheitVersKelvin (double tFarenheit) {
+        tFarenheit=((tFarenheit-32)*5/9)+273;
+        return tFarenheit;
+    }
+    
+    
+}
---
+++ TP2_convertisseurObjet_VANLERBERGHE/src/tp2_convertisseurobjet_vanlerberghe/TP2_convertisseurObjet_VANLERBERGHE.java
@@ -0,0 +1,21 @@
+/*
+ * TP2 Exercice 2
+ * TDB
+ * Octobre 
+ */
+package tp2_convertisseurobjet_vanlerberghe;
+
+/**
+ *
+ * @author louison_vl
+ */
+public class TP2_convertisseurObjet_VANLERBERGHE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Convertisseur temperature=new Convertisseur();
+    }
+    
+}
