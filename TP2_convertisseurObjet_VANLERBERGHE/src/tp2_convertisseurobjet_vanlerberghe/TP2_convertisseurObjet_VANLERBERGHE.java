aff88a17dc1b000d863b09e849b0bbfc01a19970
louison_vl <louison_vl@localhost>
2024-10-16 11:03:21 +0200

    exercice 2 TP2 terminé

--- TP2_convertisseurObjet_VANLERBERGHE/src/tp2_convertisseurobjet_vanlerberghe/TP2_convertisseurObjet_VANLERBERGHE.java
+++ TP2_convertisseurObjet_VANLERBERGHE/src/tp2_convertisseurobjet_vanlerberghe/TP2_convertisseurObjet_VANLERBERGHE.java
@@ -19,6 +19,7 @@ public class TP2_convertisseurObjet_VANLERBERGHE {
     public static void main(String[] args) {
         
         // On demande à l'utilisateur d'indiquer la température qu'il souhaite convertir
+        
         Scanner temp=new Scanner(System.in);
         System.out.println("Bonjour, saisissez une valeur : ");
         double nombre;
@@ -26,18 +27,47 @@ public class TP2_convertisseurObjet_VANLERBERGHE {
         
         // Crétaion d'un nouvel objet et conversion de la température indiquer précédemment
         Convertisseur temperature=new Convertisseur();
-        temperature.CelciusVersKelvin(nombre);
-        temperature.KelvinVersFarenheit(nombre);
-        temperature.FarenheitVersCelcius(nombre);
+        
+        String degre="";
+        double conversion=1;
+        while (conversion!=0) {
+            // On demande à l'utilisateur une conversion qu'il souhaite faire
+            Scanner conv=new Scanner(System.in);
+            System.out.println("Saisissez la conversion que vous souhaiter effectuer :"+"\n"+"1) De Celcius vers Kelvin"+"\n"+"2) De Kelvin vers Celcius");
+            System.out.println("3) De Celcius vers Farenheit"+"\n"+"4) De Farenheit vers Celcius"+"\n"+"5) De Kelvin vers Farenheit"+"\n"+"6) De Farenheit vers Kelvin"+"\n"+"0) Plus besoin de convertir");
+            conversion=conv.nextDouble();
+            
+            // En fonction de la conversion choisis, une conversion va être effectué
+            if (conversion==1) {
+                nombre=temperature.CelciusVersKelvin(nombre);
+                degre="Kelvin";
+                System.out.println(nombre);
+            } else if (conversion==2) {
+                nombre=temperature.KelvinVersCelcius(nombre);
+                degre="Celcius";
+                System.out.println(nombre);
+            } else if (conversion==3) {
+                nombre=temperature.CelciusVersFarenheit(nombre);
+                degre="Farenheit";
+                System.out.println(nombre);
+            } else if (conversion==4) {
+                nombre=temperature.FarenheitVersCelcius(nombre);
+                degre="Celcius";
+                System.out.println(nombre);
+            } else if (conversion==5) {
+                nombre=temperature.KelvinVersFarenheit(nombre);
+                degre="Farenheit";
+                System.out.println(nombre);
+            } else if (conversion==6) {
+                nombre=temperature.FarenheitVersKelvin(nombre);
+                degre="Kelvin";
+                System.out.println(nombre);
+            }
+        }
+        System.out.println(nombre+degre);
         System.out.println(temperature);
         
         
-        // On demande à l'utilisateur une conversion qu'il souhaite faire
-        Scanner conv=new Scanner(System.in);
-        System.out.println("Saisissez la conversion que vous souhaiter effectuer :"+"\n"+"1) De Celcius vers Kelvin"+"\n"+"2) De Kelvin vers Celcius");
-        System.out.println("3) De Celcius vers Farenheit"+"\n"+"4) De Farenheit vers Celcius"+"\n"+"5) De Kelvin vers Farenheit"+"\n"+"6) De Farenheit vers Kelvin"+"\n"+"0) Plus besoin de convertir");
-        double conversion;
-        conversion=conv.nextDouble();
     }
     
 }
