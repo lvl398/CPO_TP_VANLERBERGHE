68dc8871754969c40224919225dca7ecd56d6a72
louison_vl <louison_vl@localhost>
2024-10-22 10:29:11 +0200

    TP2 Exercice 3

---
+++ TP2_maniip_VANLERBERGHE/src/tp2_maniip_vanlerberghe/TP2_maniip_VANLERBERGHE.java
@@ -0,0 +1,21 @@
+/*
+ * TD2 Exercice 3
+ * TDB
+ * Octobre 2024
+ */
+package tp2_maniip_vanlerberghe;
+
+/**
+ *
+ * @author louison_vl
+ */
+public class TP2_maniip_VANLERBERGHE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ TP2_maniip_VANLERBERGHE/src/tp2_maniip_vanlerberghe/Tartiflette.java
@@ -0,0 +1,14 @@
+/*
+ * TD2 Exercice 3
+ * TDB
+ * Octobre 2024
+ */
+package tp2_maniip_vanlerberghe;
+
+/**
+ *
+ * @author louison_vl
+ */
+public class Tartiflette {
+    
+}
